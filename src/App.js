ab8def3e911523dd1743c11f4c55aba42dbe18a6
guuiij <guilherme.jadson@hotmail.com>
2025-01-02 11:36:34 -0300

    feat(components): alterando estado da aplicação

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Luffy, Rei dos piratas</h1>
-      <Comentario nome="Zoro" email="zoro@gmail.com" date={new Date(2024, 3, 19)}>
-        Onde estou ?
-      </Comentario>
-      <Comentario nome="Nami" email="nami@gmail.com" date={new Date(2018, 9, 18)}>
-        Vou ficar rica !
-      </Comentario >
-    </div>
-  );
+class App extends Component {
+
+  //aqui é o estado da aplicação
+  state = {     // Forma atual de fazer é com Hook, utilizando esse apra aprendizado
+    comentarios: [
+      {
+        nome: 'Brook',
+        email: 'brook@gmail.com',
+        date: new Date(2025, 1, 1),
+        mensagem: 'Yo ho-ho~',
+      },
+      {
+        nome: 'Chopper',
+        email: 'chopper@gmail.com',
+        date: new Date(2025, 1, 1),
+        mensagem: 'Eu sou um médico',
+      }
+    ]
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Luffy, Rei dos piratas</h1>
+
+        {this.state.comentarios.map((comentario, indice) => (
+          <Comentario
+            key={indice}
+            nome={comentario.nome}
+            email={comentario.email}
+            date={comentario.date}>
+            {comentario.mensagem}
+          </Comentario>
+
+        ))}
+
+
+        {/*<Comentario nome="Nami" email="nami@gmail.com" date={new Date(2018, 9, 18)}>
+          Vou ficar rica !
+        </Comentario >*/}
+      </div>
+    );
+  }
+
 }
 
 export default App;
