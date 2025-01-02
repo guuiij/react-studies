021d55b6077d21d2dd843eadd575d2ea954241db
Guilherme <guilherme.jadson@hotmail.com>
2025-01-01 23:30:02 -0300

    Initial commit: Configuração inicial do repositório e Criando componentes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Comentario from './components/Comentario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Luffy, Rei dos piratas</h1>
+      <Comentario />
+      <Comentario />
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comentario.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Comentario.css'
+
+
+//JSX   -- sintaxe que usa dentro do HTML, precisa importar
+const Comentario = () => (
+    <div className="Comentario">
+        <div>Zoro</div>
+        <div>Olá, tudo bem?!</div>
+    </div>
+
+)
+
+
+export default Comentario; // pode ser usado em outros lugares
